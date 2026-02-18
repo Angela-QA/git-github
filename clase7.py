e5de2094335a56d524cd2b95ff314752bf115fa4
AngelaQA <aimendz@hotmail.com>
2026-02-18 05:35:40 -0300

    Agregue archivo clase7.py a develop

---
+++ clase7.py
@@ -0,0 +1,19 @@
+lista_nombre=[]
+nombre= input("INgrese el nombre del cliente. Para terminar escriba 'fin'. ").strip()
+
+while nombre.lower() != "fin":
+    
+    if nombre != "":
+       lista_nombre.append(nombre.title())
+    else:
+        print("El nombre está vacío. POr favor ingreselo nuevamente:")
+    nombre= input("INgrese el nombre del cliente. Para terminar escriba 'fin'. ").strip()
+    
+lista_nombre.sort()
+print("Lista de nombres ordenadas alfabeticamente. ")
+print("*******************************************")
+for nombres in lista_nombre:
+    print( "         ",nombres)
+
+
+    
